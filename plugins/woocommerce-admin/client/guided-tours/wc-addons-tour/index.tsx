0babfd42ebf11284d54eee9fe32278bfb1531764
Michal Iwanow <4765119+mcliwanow@users.noreply.github.com>
2022-10-25 16:54:39 +0200

    Add track events to In-App Marketplace tour
    Track events were added to the following actions:
    - tour started
    - next step viewed
    - tour dismissed
    - tour completed

--- plugins/woocommerce-admin/client/guided-tours/wc-addons-tour/index.tsx
+++ plugins/woocommerce-admin/client/guided-tours/wc-addons-tour/index.tsx
@@ -3,6 +3,7 @@
  */
 import { useEffect, useState } from '@wordpress/element';
 import { TourKit, TourKitTypes } from '@woocommerce/components';
+import { recordEvent } from '@woocommerce/tracks';
 import { useDispatch } from '@wordpress/data';
 import { OPTIONS_STORE_NAME } from '@woocommerce/data';
 import qs from 'qs';
@@ -12,15 +13,41 @@ import qs from 'qs';
  */
 import { waitUntilElementTopNotChange } from '../utils';
 import { getTourConfig } from './get-config';
+import { getSteps } from './get-steps';
 
 const WCAddonsTour = () => {
 	const [ showTour, setShowTour ] = useState( false );
-	const [ tourConfig, setTourConfig ] = useState< TourKitTypes.WooConfig >();
-
 	const { updateOptions } = useDispatch( OPTIONS_STORE_NAME );
 
+	const steps = getSteps();
+	const defaultAutoScrollBlock: ScrollLogicalPosition = 'center';
+
+	useEffect( () => {
+		const query = qs.parse( window.location.search.slice( 1 ) );
+		if ( query?.tutorial === 'true' ) {
+			const intervalId = waitUntilElementTopNotChange(
+				steps[ 0 ].referenceElements?.desktop || '',
+				() => {
+					const stepName = steps[ 0 ]?.meta?.name;
+					setShowTour( true );
+					recordEvent( 'in_app_marketplace_tour_started', {
+						step: stepName,
+					} );
+				},
+				500
+			);
+			return () => clearInterval( intervalId );
+		}
+		// only run once
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [] );
+
+	if ( ! showTour ) {
+		return null;
+	}
+
 	const closeHandler: TourKitTypes.CloseHandler = (
-		steps,
+		tourSteps,
 		currentStepIndex
 	) => {
 		setShowTour( false );
@@ -34,54 +61,35 @@ const WCAddonsTour = () => {
 		window.history.replaceState( null, '', url );
 
 		if ( steps.length - 1 === currentStepIndex ) {
-			// TODO: Track Tour as completed
+			recordEvent( 'in_app_marketplace_tour_completed' );
 		} else {
-			// TODO: Track Tour as dismissed
+			const stepName = tourSteps[ currentStepIndex ]?.meta?.name;
+			recordEvent( 'in_app_marketplace_tour_dismissed', {
+				step: stepName,
+			} );
 		}
 	};
 
-	const onNextStepHandler = ( stepIndex: number ) => {
-		// const stepName = tourConfig?.steps[ stepIndex ]?.meta?.name || '';
-		// TODO: Maybe scroll to a proper element
-		// TODO: Track tour's step as completed
-	};
-
-	const onPreviousStepHandler = ( stepIndex: number ) => {
-		// TODO: Maybe scroll to a proper element
-	};
-
-	const defaultAutoScrollBlock: ScrollLogicalPosition = 'center';
-
-	useEffect( () => {
-		const theConfig = getTourConfig( {
-			closeHandler,
-			onNextStepHandler,
-			onPreviousStepHandler,
-			autoScrollBlock: defaultAutoScrollBlock,
+	const onNextStepHandler = ( previousStepIndex: number ) => {
+		const stepName = steps[ previousStepIndex + 1 ]?.meta?.name || '';
+		recordEvent( 'in_app_marketplace_tour_step_viewed', {
+			step: stepName,
 		} );
+		// TODO: Maybe scroll to a proper element
+	};
 
-		setTourConfig( theConfig );
+	const onPreviousStepHandler = ( previousStepIndex: number ) => {
+		// TODO: Maybe scroll to a proper element
+		// const step = steps[ previousStepIndex - 1 ];
+	};
 
-		const query = qs.parse( window.location.search.slice( 1 ) );
-		if ( query?.tutorial === 'true' ) {
-			const intervalId = waitUntilElementTopNotChange(
-				theConfig.steps[ 0 ].referenceElements?.desktop || '',
-				() => {
-					setShowTour( true );
-
-					// TODO: track the tour is started
-				},
-				500
-			);
-			return () => clearInterval( intervalId );
-		}
-		// only run once
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [] );
-
-	if ( ! showTour || ! tourConfig ) {
-		return null;
-	}
+	const tourConfig = getTourConfig( {
+		closeHandler,
+		onNextStepHandler,
+		onPreviousStepHandler,
+		autoScrollBlock: defaultAutoScrollBlock,
+		steps,
+	} );
 
 	return <TourKit config={ tourConfig } />;
 };
